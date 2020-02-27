85888a818b5a72b49aa28eb158caf56182602cc8
Jack <j0atm0n@protonmail.com>
2020-02-26 17:17:09 -0700

    Fixed ban command

--- commands/ban.js
+++ commands/ban.js
@@ -2,50 +2,44 @@ module.exports = {
 	name: 'ban',
 	description: 'Bans a user for optional time and reason.',
 	usage: '<@mention> [time] [reason]',
-	execute(message, args)
-	{
-		if(!message.member.hasPermission('BAN_MEMBERS', false, true, true))
-		{
+	execute(message, args) {
+		if(!message.member.hasPermission('BAN_MEMBERS', false, true, true)) {
 			message.reply('you don\'t have permission to ban members!');
 			return;
 		}
-		
-		let member = message.mentions.members.first();
-		if(!member)
-		{
+		const guild = message.guild;
+		const member = message.mentions.members.first();
+		if(!member) {
 			return message.reply('please mention a valid user.');
 		}
-		
-		let time = args[1];
-		if(time.length == 2 && Number(time.charAt(0) != NaN))
-		{
-			let length = Number(time.charAt(0));
-			switch(time.charAt(1))
-			{
-				case 's':
-				setTimeout( () => {guild.unban(member, 'Ban timeout expired, auto unban.')}, length * 1000);
+
+		const time = args[1];
+		if(time.length == 2 && !isNaN(Number(time.charAt(0)))) {
+			const length = Number(time.charAt(0));
+			switch(time.charAt(1)) {
+			case 's':
+				setTimeout(() => {guild.unban(member, 'Ban timeout expired, auto unban.');}, length * 1000);
 				break;
-				
-				case 'm':
-				setTimeout( () => {guild.unban(member, 'Ban timeout expired, auto unban.')}, length * 60000);
+
+			case 'm':
+				setTimeout(() => {guild.unban(member, 'Ban timeout expired, auto unban.');}, length * 60000);
 				break;
-				
-				case 'h':
-				setTimeout( () => {guild.unban(member, 'Ban timeout expired, auto unban.')}, length * 3600000);
+
+			case 'h':
+				setTimeout(() => {guild.unban(member, 'Ban timeout expired, auto unban.');}, length * 3600000);
 				break;
-				
-				case 'd':
-				setTimeout( () => {guild.unban(member, 'Ban timeout expired, auto unban.')}, length * 86400000);
+
+			case 'd':
+				setTimeout(() => {guild.unban(member, 'Ban timeout expired, auto unban.');}, length * 86400000);
 				break;
-				
-				default:
+
+			default:
 				return message.reply `\`time\` argument not formatteed correctly!`;
 			}
-			
-			let reason = args.slice(2).join(' ');
+
+			const reason = args.slice(2).join(' ');
 		}
-		else
-		{
+		else {
 			reason = args.slice(1).join(' ');
 		}
 		member.ban();
